a0c72db06249c4a6c876904e500c7ffc82f16cf6
Bruno Santos <brunosantosengmec@gmail.com>
2020-01-22 22:06:19 -0300

    Conexão com o banco de dados do firebase

---
+++ js/init.js
@@ -0,0 +1,12 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyCpJtEoVRFJBU7VFFVpuaKU_cjTO14RZtc",
+    authDomain: "linkme-7e85b.firebaseapp.com",
+    databaseURL: "https://linkme-7e85b.firebaseio.com",
+    projectId: "linkme-7e85b",
+    storageBucket: "linkme-7e85b.appspot.com",
+    messagingSenderId: "122655489564",
+    appId: "1:122655489564:web:d259e7e7c0f0287c5029d9"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
